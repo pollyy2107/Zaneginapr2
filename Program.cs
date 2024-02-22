7d261d3fc3478c97c58fee290af2f15c13cc2769
almazikk999 <almazikk999@vk.com>
2024-02-22 10:40:06 +0300

    добавление коммита

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZaneginaGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 5;
+            Console.WriteLine($"Результат: {a * b}");
+        }
+    }
+}
